104c1d7a9636c41a4851929d8b60cac61291405e
Pedro <pedrh315@gmail.com>
2023-03-26 20:48:56 -0300

    adição de link para o github do repositório

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,19 +13,21 @@ const Header = () => {
             {
                 schedullingData ? 
                 <>
+                    <div>
                         <h1 className="scheduled-state-title"> Data de agendamento: </h1>
                         <p>
                             <span className="day">{schedullingData.day}</span>
                             <span className="month">{schedullingData.month}</span>
                             <span className="year">{schedullingData.year}</span>
                         </p>
+                    </div>
                 </>
                     :
                 <h1>
                     Faça seu agendamento!
                 </h1>
             }
-
+            <p className="github-link"><strong>Github do repositório: <a href="https://github.com/Pedrh316/Agenda.git" target="_blank">https://github.com/Pedrh316/Agenda.git</a></strong></p>
         </header>
     )
 }
